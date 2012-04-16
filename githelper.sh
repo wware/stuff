8f7031852c4852c0c5237595683c31a57c3da1e6
Will Ware <wware@litl.com>
2012-04-15 22:53:46 -0400

    use an environment variable here

--- githelper.sh
+++ githelper.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
 
-GOAL=goal
-#GOAL=fixes-2
-
 if [ "X" == "X$1" ]
 then
     git show HEAD | grep '^diff' | sed 's#.*b/##'
 else
+    if [ "X" == "X${GITGOAL}" ]
+    then
+        echo Please set GITGOAL environment variable.
+        echo It should identify the commit you\'re moving toward.
+    fi
     for x in $@
     do
-	echo "git show ${GOAL}:$x > /tmp/zz"
-	git show ${GOAL}:$x > /tmp/zz
+	echo "git show ${GITGOAL}:$x > /tmp/zz"
+	git show ${GITGOAL}:$x > /tmp/zz
 	echo "meld /tmp/zz $x"
 	meld /tmp/zz $x
     done
