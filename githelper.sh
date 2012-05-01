03a411ff48f42a095257b1501c47d66a273179be
Will Ware <wware@litl.com>
2012-05-01 17:27:56 -0400

    improved

--- githelper.sh
+++ githelper.sh
@@ -3,6 +3,14 @@
 if [ "X" == "X$1" ]
 then
     git show HEAD | grep '^diff' | sed 's#.*b/##'
+elif [ "Xid" == "X$1" ]
+then
+    echo -n "Change-Id: I"
+    for i in  {1..10}
+    do
+        printf "%04x" $((RANDOM % 65536))
+    done
+    echo
 else
     if [ "X" == "X${GITGOAL}" ]
     then
