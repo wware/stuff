b0ee36cdb7207caa2d4096dc6ef7f543b0d726de
Will Ware <wware@alum.mit.edu>
2012-12-20 10:42:27 -0500

    more fun with rdflib

--- semweb/hack-rdflib.py
+++ semweb/hack-rdflib.py
@@ -1,23 +1,145 @@
-from rdflib.Graph import Graph
-from rdflib import Namespace
+from rdflib import Graph, Namespace, Literal, BNode, RDF, URIRef
+import plugin
+import sys
+import string
+import getopt
 
-# Create a graph and pull some factoids into it.
-g = Graph()
-g.parse("http://bigasterisk.com/foaf.rdf")  # FOAF info for Drew Perttula
-g.parse("http://www.w3.org/People/Berners-Lee/card.rdf")  # TBL's v-card
+DEBUG = False
 
-FOAF = Namespace(
-    "http://xmlns.com/foaf/0.1/"   # this works
-    #"http://xmlns.com/foaf/spec/20071002.rdf"  # this DOES NOT work
-    )
-g.parse("http://danbri.livejournal.com/data/foaf") # Several friends of Dan Brickley
-# treat all member_names as regular names
-[g.add((s, FOAF['name'], n)) for s,_,n in g.triples((None, FOAF['member_name'], None))]
+def hasSubstring(haystack, needle):
+    # case-insenstive
+    try:
+        unicode(haystack).lower().index(needle.lower())
+        return True
+    except ValueError:
+        return False
 
-# I can't get anything when I try to fetch http://xmlns.com/foaf/0.1/
-# I just get redirected to http://xmlns.com/foaf/spec/
-# So I'm not sure what people are doing there
+class GraphLoader:
+    def __init__(self, starter, maxcount=1000):
+        self.graph = Graph()
+        #self.graph.bind("dc", "http://http://purl.org/dc/elements/1.1/")
+        #self.graph.bind("foaf", "http://xmlns.com/foaf/0.1/")
+        self.already = set()
+        self.queue = [ starter ]
+        while len(self.graph) < maxcount:
+            if not self.next():
+                print 'OUCH'
+                break
+            self.explore()
+    def enqueue(self, lst):
+        for x in lst:
+            self.queue.append(x)
+    def subjects(self):
+        return list(set(map(lambda spo: spo[0], self.graph)))
+    def predicates(self):
+        return list(set(map(lambda spo: spo[1], self.graph)))
+    def objects(self):
+        return list(set(map(lambda spo: spo[2], self.graph)))
+    def badSubstrings(self):
+        return (
+            "http://dbpedia.org/ontology/wikiPage",
+            "http://dbpedia.org/property/wikiPage",
+            #"dbpprop:wikiPage",
+            ".dbpedia.org" # international DBpedias are unreliable
 
-for row in g.query('SELECT ?aname ?bname WHERE { ?a foaf:knows ?b . ?a foaf:name ?aname . ?b foaf:name ?bname . }',
-                   initNs=dict(foaf=Namespace("http://xmlns.com/foaf/0.1/"))):
-    print "%s knows %s" % row
+            # I don't usually care about all this ontological stuff.
+            "http://purl.org/dc/",
+            "http://xmlns.com/foaf",
+            "http://www.w3.org/1999/02/22-rdf",
+            "http://www.w3.org/1999/xhtml/vocab",
+            "http://www.w3.org/2000/01/rdf-schema",
+            "http://www.w3.org/2002/07/owl",
+            "http://www.w3.org/2002/07/owl",
+            "http://www.w3.org/2003/06/sw-vocab-status/"
+            "http://www.w3.org/2004/02/skos",
+            "http://www.w3.org/ns",
+            )
+    def next(self):
+        def acceptable(spo,badSubstrings=self.badSubstrings()):
+            for x in spo:
+                for badness in badSubstrings:
+                    if hasSubstring(unicode(x), badness):
+                        return False
+            return True
+        while len(self.queue) > 0:
+            x = unicode(self.queue.pop(0))
+            if x not in self.already:
+                self.already.add(x)
+                try:
+                    self.graph.parse(x)
+                    oldgraph = self.graph
+                    self.graph = Graph()
+                    for spo in oldgraph:
+                        if acceptable(spo):
+                            self.graph.add(spo)
+                    if DEBUG:
+                        print x, len(self.graph)
+                    return True
+                except KeyboardInterrupt:
+                    sys.exit(0)
+                except Exception, e:
+                    #print e
+                    pass
+        return False
+    def explore(self):
+        for spo in self.graph:
+            for x in spo:
+                x = unicode(x)
+                if x not in self.queue:
+                    self.queue.append(x)
+    def rdfxml(self):
+        return self.graph.serialize()
+    def turtle(self):
+        return self.graph.serialize(format='turtle')
+
+def usage ():
+    print('Usage')
+
+def main():
+    global DEBUG
+    try:
+        opts, args = getopt.getopt(sys.argv[1:], 'rdc:')
+    except getopt.GetoptError as err:
+        print(err)
+        usage()
+        sys.exit()
+
+    searchArg = None
+    maxcount = 100
+    for o, a in opts:
+        if o in ("-r", "--read"):
+            searchArg = raw_input("Search term: ").decode(sys.stdin.encoding)
+        elif o in ("-d", "--debug"):
+            DEBUG = True
+        elif o in ("-c", "--maxcount"):
+            maxcount = string.atoi(a)
+        else:
+            usage()
+            sys.exit()
+    if searchArg is not None or len(args) > 0:
+        if len(args) > 0:
+            searchArg = args[0]
+        if DEBUG:
+            print "http://dbpedia.org/resource/" + unicode(searchArg.replace(" ","_"))
+        print GraphLoader("http://dbpedia.org/resource/" +
+                          searchArg.replace(" ","_"), maxcount=maxcount).turtle()
+    else:
+        # Create a graph and pull some factoids into it.
+        g = Graph()
+        g.parse("http://bigasterisk.com/foaf.rdf")  # FOAF info for Drew Perttula
+        g.parse("http://www.w3.org/People/Berners-Lee/card.rdf")  # TBL's v-card
+
+        FOAF = Namespace("http://xmlns.com/foaf/0.1/")
+        g.parse("http://danbri.livejournal.com/data/foaf") # Several friends of Dan Brickley
+        [g.add((s, FOAF['name'], n)) for s,_,n in g.triples((None, FOAF['member_name'], None))]
+
+        if True:
+            for row in g.query('SELECT ?aname ?bname WHERE { ' +
+                               '?a foaf:knows ?b . ?a foaf:name ?aname . ?b foaf:name ?bname . }',
+                               initNs=dict(foaf=Namespace("http://xmlns.com/foaf/0.1/"))):
+                print "%s knows %s" % row
+        else:
+            print g.serialize(format='turtle')
+
+if __name__ == '__main__':
+    main()
---
+++ semweb/plugin.py
@@ -0,0 +1,150 @@
+"""
+Plugin support for rdf.
+
+There are a number of plugin points for rdf: parser, serializer,
+store, query processor, and query result. Plugins can be registered
+either through setuptools entry_points or by calling
+rdf.plugin.register directly.
+
+If you have a package that uses a setuptools based setup.py you can add the following to your setup::
+
+    entry_points = {
+        'rdf.plugins.parser': [
+            'nt =     rdf.plugins.parsers.NTParser:NTParser',
+            ],
+        'rdf.plugins.serializer': [
+            'nt =     rdf.plugins.serializers.NTSerializer:NTSerializer',
+            ],
+        }
+
+See the `setuptools dynamic discovery of services and plugins <http://peak.telecommunity.com/DevCenter/setuptools#dynamic-discovery-of-services-and-plugins> for more information.
+
+"""
+
+from rdflib.store import Store
+from rdflib.serializer import Serializer
+from rdflib.parser import Parser
+from rdflib.exceptions import Error
+from rdfextras import sparql
+#from rdflib.query.result import QueryResult
+from rdflib.query import Processor, Result
+
+
+entry_points = {'rdf.plugins.store': Store,
+                'rdf.plugins.serializer': Serializer,
+                'rdf.plugins.parser': Parser}
+
+_plugins = {}
+
+
+class PluginException(Error):
+    pass
+
+
+class Plugin(object):
+
+    def __init__(self, name, kind, module_path, class_name):
+        self.name = name
+        self.kind = kind
+        self.module_path = module_path
+        self.class_name = class_name
+        self._class = None
+
+    def getClass(self):
+        if self._class is None:
+            module = __import__(self.module_path, globals(), locals(), [""])
+            self._class = getattr(module, self.class_name)
+        return self._class
+
+
+class PKGPlugin(Plugin):
+
+    def __init__(self, name, kind, ep):
+        self.name = name
+        self.kind = kind
+        self.ep = ep
+        self._class = None
+
+    def getClass(self):
+        if self._class is None:
+            self._class = self.ep.load()
+        return self._class
+
+
+def register(name, kind, module_path, class_name):
+    """
+    Register the plugin for (name, kind). The module_path and
+    class_name should be the path to a plugin class.
+    """
+    p = Plugin(name, kind, module_path, class_name)
+    _plugins[(name, kind)] = p
+
+
+def get(name, kind):
+    """
+    Return the class for the specified (name, kind). Raises a
+    PluginException if unable to do so.
+    """
+    try:
+        p = _plugins[(name, kind)]
+    except KeyError, e:
+        raise PluginException("No plugin registered for (%s, %s)" % (name, kind))
+    return p.getClass()
+
+
+try:
+    from pkg_resources import iter_entry_points
+except ImportError:
+    pass # TODO: log a message
+else:
+    # add the plugins specified via pkg_resources' EntryPoints.
+    for entry_point, kind in entry_points.iteritems():
+        for ep in iter_entry_points(entry_point):
+            _plugins[(ep.name, kind)] = PKGPlugin(ep.name, kind, ep)
+
+
+def plugins(name=None, kind=None):
+    """
+    A generator of the plugins.
+
+    Pass in name and kind to filter... else leave None to match all.
+    """
+    for p in _plugins.values():
+        if (name is None or name==p.name) and (kind is None or kind==p.kind):
+            yield p
+
+register('default', Store, 'rdflib.store.IOMemory', 'IOMemory')
+register('IOMemory', Store, 'rdflib.store.IOMemory', 'IOMemory')
+register('BerkeleyDB', Store, 'rdflib.store.BerkeleyDB', 'BerkeleyDB')
+register('Sleepycat', Store, 'rdflib.store.Sleepycat', 'Sleepycat')
+register('BDBOptimized', Store, 'rdflib.store.BDBOptimized', 'BDBOptimized')
+register('MySQL', Store, 'rdflib.store.MySQL', 'MySQL')
+register('PostgreSQL', Store, 'rdflib.store.MySQL', 'PostgreSQL')
+register('SQLite', Store, 'rdflib.store.SQLite', 'SQLite')
+
+register('xml', Serializer, 'rdflib.syntax.serializers.XMLSerializer',
+         'XMLSerializer')
+register('n3', Serializer, 'rdflib.syntax.serializers.N3Serializer',
+         'N3Serializer')
+register('turtle', Serializer, 'rdflib.syntax.serializers.TurtleSerializer',
+         'TurtleSerializer')
+register('nt', Serializer, 'rdflib.syntax.serializers.NTSerializer',
+         'NTSerializer')
+register('pretty-xml', Serializer,
+        'rdflib.syntax.serializers.PrettyXMLSerializer', 'PrettyXMLSerializer')
+register('trix', Serializer,
+        'rdflib.syntax.serializers.TriXSerializer', 'TriXSerializer')
+
+register('application/rdf+xml', Parser,
+        'rdflib.syntax.parsers.RDFXMLParser', 'RDFXMLParser')
+register('xml', Parser, 'rdflib.syntax.parsers.RDFXMLParser', 'RDFXMLParser')
+register('n3', Parser, 'rdflib.syntax.parsers.notation3', 'N3Parser')
+register('nt', Parser, 'rdflib.syntax.parsers.NTParser', 'NTParser')
+register('trix', Parser, 'rdflib.syntax.parsers.TriXParser', 'TriXParser')
+register('rdfa', Parser, 'rdflib.syntax.parsers.rdfa', 'RDFaParser')
+
+register('sparql', Processor,
+         'rdflib.sparql.bison.Processor', 'Processor')
+register('SPARQLQueryResult',Result,
+         'rdflib.sparql.QueryResult', 'SPARQLQueryResult')
+
