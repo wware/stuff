2b620dd3ba0db33c0899d41e1871a8a8e18bfeb8
Will Ware <wware@alum.mit.edu>
2012-12-21 15:48:32 -0500

    a little more puttering

--- semweb/hack-rdflib.py
+++ semweb/hack-rdflib.py
@@ -3,24 +3,50 @@ import plugin
 import sys
 import string
 import getopt
+import pprint
 
 DEBUG = False
+
+badSubstrings = (
+    "http://dbpedia.org/ontology/wikiPage",
+    "http://dbpedia.org/property/wikiPage",
+    #"dbpprop:wikiPage",
+    ".dbpedia.org" # international DBpedias are unreliable
+
+    # I don't usually care about all this ontological stuff.
+    "http://purl.org/dc/",
+    "http://xmlns.com/foaf",
+    "http://www.w3.org/1999/02/22-rdf",
+    "http://www.w3.org/1999/xhtml/vocab",
+    "http://www.w3.org/2000/01/rdf-schema",
+    "http://www.w3.org/2002/07/owl",
+    "http://www.w3.org/2002/07/owl",
+    "http://www.w3.org/2003/06/sw-vocab-status/"
+    "http://www.w3.org/2004/02/skos",
+    "http://www.w3.org/ns",
+    )
 
 def hasSubstring(haystack, needle):
     # case-insenstive
     try:
-        unicode(haystack).lower().index(needle.lower())
+        unicode(haystack).lower().index(unicode(needle).lower())
         return True
     except ValueError:
         return False
 
+def goodTriple(spo):
+    for y in spo:
+        for badness in badSubstrings:
+            if hasSubstring(y, badness):
+                return False
+    return True
+
 class GraphLoader:
     def __init__(self, starter, maxcount=1000):
         self.graph = Graph()
-        #self.graph.bind("dc", "http://http://purl.org/dc/elements/1.1/")
-        #self.graph.bind("foaf", "http://xmlns.com/foaf/0.1/")
         self.already = set()
         self.queue = [ starter ]
+        self.already.add(starter)
         while len(self.graph) < maxcount:
             if not self.next():
                 print 'OUCH'
@@ -28,65 +54,43 @@ class GraphLoader:
             self.explore()
     def enqueue(self, lst):
         for x in lst:
-            self.queue.append(x)
+            if x not in self.already:
+                self.already.add(x)
+                self.queue.append(x)
     def subjects(self):
         return list(set(map(lambda spo: spo[0], self.graph)))
     def predicates(self):
         return list(set(map(lambda spo: spo[1], self.graph)))
     def objects(self):
         return list(set(map(lambda spo: spo[2], self.graph)))
-    def badSubstrings(self):
-        return (
-            "http://dbpedia.org/ontology/wikiPage",
-            "http://dbpedia.org/property/wikiPage",
-            #"dbpprop:wikiPage",
-            ".dbpedia.org" # international DBpedias are unreliable
-
-            # I don't usually care about all this ontological stuff.
-            "http://purl.org/dc/",
-            "http://xmlns.com/foaf",
-            "http://www.w3.org/1999/02/22-rdf",
-            "http://www.w3.org/1999/xhtml/vocab",
-            "http://www.w3.org/2000/01/rdf-schema",
-            "http://www.w3.org/2002/07/owl",
-            "http://www.w3.org/2002/07/owl",
-            "http://www.w3.org/2003/06/sw-vocab-status/"
-            "http://www.w3.org/2004/02/skos",
-            "http://www.w3.org/ns",
-            )
     def next(self):
-        def acceptable(spo,badSubstrings=self.badSubstrings()):
-            for x in spo:
-                for badness in badSubstrings:
-                    if hasSubstring(unicode(x), badness):
-                        return False
-            return True
+        #if DEBUG: pprint.pprint(self.queue[:5])
         while len(self.queue) > 0:
             x = unicode(self.queue.pop(0))
-            if x not in self.already:
-                self.already.add(x)
-                try:
-                    self.graph.parse(x)
-                    oldgraph = self.graph
-                    self.graph = Graph()
-                    for spo in oldgraph:
-                        if acceptable(spo):
-                            self.graph.add(spo)
-                    if DEBUG:
-                        print x, len(self.graph)
-                    return True
-                except KeyboardInterrupt:
-                    sys.exit(0)
-                except Exception, e:
-                    #print e
-                    pass
+            #if DEBUG:
+            #    print 'x =', x
+            #    pprint.pprint(self.queue[:5])
+            try:
+                self.graph.parse(x)
+                # you'd think this filtering operation would be horribly
+                # non-performant but it's actually not too bad
+                newgraph = Graph()
+                for spo in list(self.graph):
+                    if goodTriple(spo):
+                        newgraph.add(spo)
+                self.graph = newgraph
+                if DEBUG:
+                    print x, len(self.graph)
+                return True
+            except KeyboardInterrupt:
+                sys.exit(0)
+            except Exception, e:
+                #print e
+                pass
         return False
     def explore(self):
         for spo in self.graph:
-            for x in spo:
-                x = unicode(x)
-                if x not in self.queue:
-                    self.queue.append(x)
+            self.enqueue(map(unicode, spo))
     def rdfxml(self):
         return self.graph.serialize()
     def turtle(self):
@@ -98,12 +102,12 @@ def usage ():
 def main():
     global DEBUG
     try:
-        opts, args = getopt.getopt(sys.argv[1:], 'rdc:')
+        opts, args = getopt.getopt(sys.argv[1:], 'rdxc:')
     except getopt.GetoptError as err:
         print(err)
         usage()
         sys.exit()
-
+    oformat = "turtle"
     searchArg = None
     maxcount = 100
     for o, a in opts:
@@ -113,6 +117,8 @@ def main():
             DEBUG = True
         elif o in ("-c", "--maxcount"):
             maxcount = string.atoi(a)
+        elif o in ("-x", "--xml"):
+            oformat = "xml"
         else:
             usage()
             sys.exit()
@@ -121,8 +127,13 @@ def main():
             searchArg = args[0]
         if DEBUG:
             print "http://dbpedia.org/resource/" + unicode(searchArg.replace(" ","_"))
-        print GraphLoader("http://dbpedia.org/resource/" +
-                          searchArg.replace(" ","_"), maxcount=maxcount).turtle()
+
+        g = GraphLoader("http://dbpedia.org/resource/" +
+                        searchArg.replace(" ","_"), maxcount=maxcount)
+        if oformat == 'turtle':
+            print g.turtle()
+        else:
+            print g.rdfxml()
     else:
         # Create a graph and pull some factoids into it.
         g = Graph()
