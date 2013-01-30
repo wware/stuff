cc5a3f90b824cf4388e3f498eadbd3207b24761f
Will Ware <wware@alum.mit.edu>
2013-01-30 09:53:54 -0500

    updates from the server

--- semantic-wiki/rdf-cgi.py
+++ semantic-wiki/rdf-cgi.py
@@ -1,17 +1,18 @@
 #!/usr/bin/python
-
 import cgi
 import os
 import re
 import urllib
+import HTMLParser
+
+DEBUG=False
 
 print "Content-Type: plain/text"
 print
-
-r1 = re.compile("<textarea readonly[^>]*>")
+r1 = re.compile("<textarea [^>]*>")
 r2 = re.compile("</textarea>")
-r3 = re.compile("===? (rdf|RDF) ===?")
-r4 = re.compile("title=[A-Z][_a-zA-Z0-9]*")
+r3 = re.compile("=+ (rdf|RDF) =+")
+r4 = re.compile("title=[A-Z][_:a-zA-Z0-9]*")
 
 pagename = "Main_Page"
 if os.environ.has_key('QUERY_STRING'):
@@ -20,19 +21,25 @@ if os.environ.has_key('QUERY_STRING'):
     if m is not None:
         pagename = qs[m.start()+6:m.end()]
 
+if DEBUG:
+    print 'pagename=' + pagename
+
 url = "http://localhost/wiki/index.php?title=" + pagename + "&action=edit"
 R = urllib.urlopen(url).read()
-R = R[r1.search(R).end():r2.search(R).start()].split('\n')
+R = R[r1.search(R).end():r2.search(R).start()]
+R = HTMLParser.HTMLParser().unescape(R).split('\n')
+if DEBUG:
+    print R
+    print '=========================='
 
 rdfState = 0
 for L in R:
     m = r3.search(L)
     if rdfState == 0 and m is not None:
         rdfState = 1
-    elif rdfState == 1 and L[:1] == ' ':
+    elif rdfState == 1 and L == '<pre>':
         rdfState = 2
-        print L[1:]
-    elif L[:1] == ' ':
-        print L[1:]
-    else:
+    elif L == '</pre>':
         rdfState = 0
+    elif rdfState == 2:
+        print L
