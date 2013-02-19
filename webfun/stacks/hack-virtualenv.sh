b1b6de963fac6f286f28fd7f8a9777a1ed21dd5b
Will Ware <wware@alum.mit.edu>
2013-02-18 23:18:55 -0500

    refactor to eliminate duplication

--- webfun/stacks/hack-virtualenv.sh
+++ webfun/stacks/hack-virtualenv.sh
@@ -1,22 +1,25 @@
 #!/bin/bash
 
-#ENVIRONMENT=django-sqlite3
-ENVIRONMENT=$1
+# This is for a Django app with various possible database choices,
+# currently including "sqlite3" and "postgresql".
+
+WHICHDB=$1
 DJANGOPROJECT=trivialproject
 
-virtualenv --no-site-packages ${ENVIRONMENT}
-cd ${ENVIRONMENT}
+virtualenv --no-site-packages django-app
+(
+cd django-app
 source bin/activate
 
-pip install -r ../requirements.txt || exit 1
+pip install -r ../requirements-${WHICHDB}.txt || exit 1
 
 # Tweak the settings file, but keep the secret key
 (
-M=$(wc ../settings-${ENVIRONMENT}.py | cut -c -8)
-N=$(grep -n SECRET_KEY ../settings-${ENVIRONMENT}.py | sed 's/:.*//')
-head -$((N-1)) ../settings-${ENVIRONMENT}.py
+M=$(wc ../settings-django-${WHICHDB}.py | cut -c -8)
+N=$(grep -n SECRET_KEY ../settings-django-${WHICHDB}.py | sed 's/:.*//')
+head -$((N-1)) ../settings-django-${WHICHDB}.py
 python ../genkey.py
-tail -n $((M-N)) ../settings-${ENVIRONMENT}.py
+tail -n $((M-N)) ../settings-django-${WHICHDB}.py
 ) > ${DJANGOPROJECT}/settings.py
 
 cd ${DJANGOPROJECT}
@@ -27,7 +30,5 @@ python manage.py test || exit 1
 
 python manage.py runserver
 
-cd ..
-pip freeze > ../requirements.txt
-
 deactivate 
+)
