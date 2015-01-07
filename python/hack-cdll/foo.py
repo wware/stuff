216efecf79153969b43d2f1a7ad8b5c29e452fd0
Will Ware <wware@alum.mit.edu>
2015-01-07 09:48:35 -0500

    Add more details to the cdll example.

--- python/hack-cdll/foo.py
+++ python/hack-cdll/foo.py
@@ -1,19 +1,48 @@
-# https://docs.python.org/2/library/ctypes.html
+#!/usr/bin/env python
+"""
+The magic of Python's ctypes library
+https://docs.python.org/2/library/ctypes.html
 
-import os
-from ctypes import *
-
-c_code = """\
-int add(int x, int y)
-{
-	return x + y;
-}
+For *real* fun, type:
+  $ gdb `which python`
+  ...
+  (gdb) break foo.c:3
+  (gdb) run foo.py
+  Breakpoint 1, add (x=3, y=5) at foo.c:3
+  3	    return x + y;
+  (gdb) p x
+  $1 = 3
+  (gdb) p y
+  $2 = 5
 """
 
-open("foo.c", "w").write(c_code)
-os.system("gcc -dynamiclib -o libfoo.dylib foo.c")
+# Create a C file with a function to be called.
+open("foo.c", "w").write("""\
+int add(int x, int y)
+{
+    return x + y;
+}
+""")
 
-name = "./libfoo.dylib"
-lib = cdll.LoadLibrary(name) 
+# Compile the C code into a dynamic library.
+import os
+if os.popen("uname -s").read().strip() == "Darwin":
+    # OSX
+    os.system("gcc -g -dynamiclib -o libfoo.dylib foo.c")
+    name = "./libfoo.dylib"
+else:
+    # Linux
+    os.system("gcc -g -shared -o libfoo.so foo.c")
+    name = "./libfoo.so"
 
-print lib.add(3, 5)
+# Import the library using ctypes.cdll and exercise the function.
+import ctypes
+lib = ctypes.cdll.LoadLibrary(name)
+assert lib.add(3, 5) == 8
+
+# Bad argument type should fail
+try:
+    lib.add(3.14, 6.28)
+    raise Exception("non-integer argument should be rejected")
+except ctypes.ArgumentError:
+    pass
